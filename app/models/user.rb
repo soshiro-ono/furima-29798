99a1148ccf3a50caac8dcbf5e574573293f0eb6c
小野　総司郎 <ssosksono@gmail.com>
2020-10-01 21:19:59 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # バリデーションを書いていく
-  # validates :nickname,presence: true
-  # validates :family_name,/\A[ぁ-んァ-ン一-龥]/
-  # validates :last_name,/\A[ぁ-んァ-ン一-龥]/
-  # validates :family_katakana,/\A[ァ-ヶー－]+\z/
-  # validates :last_katakana,/\A[ァ-ヶー－]+\z/
-  # validates :encrypted_password,/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  # validates :encrypted_password,2kaimenonyuuryoku
-  # validates :encrypted_password,length: { minimum: 6 }
-  # validates :email,@fukumu
-  # validates :birthday,presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :family_name,format: {with:/\A[ぁ-んァ-ン一-龥]/, message:"全角で入力してください"}
+    validates :last_name,format: {with:/\A[ぁ-んァ-ン一-龥]/, message:"全角で入力してください"}
+    validates :family_katakana,format: {with:/\A[ァ-ヶー－]+\z/, message:"全角カタカナで入力してください"}
+    validates :last_katakana,format: {with:/\A[ァ-ヶー－]+\z/, message:"全角カタカナで入力してください"}
+    validates :password,format: {with:/\A[a-zA-Z0-9]+\z/,length:{minimum:6}, message:"パスワードは6文字以上で半角英数字混合で入力してください"}
+    validates :password_confirmation,format: {with:/\A[a-zA-Z0-9]+\z/,length:{minimum:6}, message:"パスワードは6文字以上で半角英数字混合で入力してください"}
+    validates :birthday
+  end
 end
+
+
+# パスワードに関してはpasswordとpassword_confirmationを使う？
+# そもそもなぜパスワードはデバイスで設定しているのにpasswordとpassword_confirmationを使うにか
+#{6,}/i
