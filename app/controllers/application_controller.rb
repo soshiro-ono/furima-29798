f38b6b17da716a4dad8437f98bf3c6cdf21aaa22
小野　総司郎 <ssosksono@gmail.com>
2020-10-01 18:17:36 +0900

    新規ユーザー登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:password,:password_confirmation,:family_name,:last_name,:family_katakana,:last_katakana,:birthday])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # バリデーションを書いていく
+  # validates :nickname,presence: true
+  # validates :family_name,/\A[ぁ-んァ-ン一-龥]/
+  # validates :last_name,/\A[ぁ-んァ-ン一-龥]/
+  # validates :family_katakana,/\A[ァ-ヶー－]+\z/
+  # validates :last_katakana,/\A[ァ-ヶー－]+\z/
+  # validates :encrypted_password,/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  # validates :encrypted_password,2kaimenonyuuryoku
+  # validates :encrypted_password,length: { minimum: 6 }
+  # validates :email,@fukumu
+  # validates :birthday,presence: true
 end
