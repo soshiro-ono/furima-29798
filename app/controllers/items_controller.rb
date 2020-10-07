8d85d425ed966379cb32ed0c374ef995161f92c1
小野　総司郎 <ssosksono@gmail.com>
2020-10-07 19:06:55 +0900

    商品一覧機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:index]
   before_action :authenticate_user!,except: [:index]
+  
   def index
     @items = Item.all.order("created_at DESC")
   end
